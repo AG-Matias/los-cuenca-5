c60f7735caa9953632987ebcd0aa340b9a36f1fc
thiagorodrigop13-coder <thiagorodrigop13@gmail.com>
2026-05-16 10:40:12 -0300

    marias b

--- adadas.py
+++ adadas.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 def rodrigay():
     return "rodrigayaca y maraca"
 
 print(rodrigay())
 
-=======
-def matigil():
-    print("soy gay")
-matigil()
->>>>>>> 0ccd27585c402a1d8c1f9b39991ca6939d7a1455
