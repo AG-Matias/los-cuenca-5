bfff444dead24c1e4806105ed695c4bafa91480b
thiagorodrigop13-coder <thiagorodrigop13@gmail.com>
2026-05-16 10:32:58 -0300

    matias bareiro

--- adadas.py
+++ adadas.py
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
+def matigil():
+    print("soy gay")
+matigil()
+=======
 def rodrigay():
     return "rodrigayaca y maraca"
 
 print(rodrigay())
 
+
+>>>>>>> 387d3f7b52e49874efc5f3fc4d079995038982ec
